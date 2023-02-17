1c780a4d9d2fdade6c60ca1888887b518ff9dad0
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-02-17 14:15:59 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
+let richting = 0
 input.onButtonPressed(Button.A, function () {
 	
 })
-input.onGesture(Gesture.Shake, function () {
-	
-})
 basic.forever(function () {
-    basic.showArrow(ArrowNames.North)
+    richting = input.compassHeading()
+    if (richting < 45) {
+        basic.showArrow(ArrowNames.North)
+    } else if (richting < 135) {
+        basic.showArrow(ArrowNames.West)
+    } else if (richting < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else {
+        basic.showArrow(ArrowNames.East)
+    }
 })
