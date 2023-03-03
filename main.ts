4400e6bb9dfb548543e8f76eff6d85d1d6c6b824
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-03-03 14:16:20 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -4,13 +4,13 @@ input.onButtonPressed(Button.A, function () {
 })
 basic.forever(function () {
     richting = input.compassHeading()
-    if (richting < 45) {
+    if (richting > 0 && richting < 315 || richting < 0 && richting > 45) {
         basic.showArrow(ArrowNames.North)
-    } else if (richting < 135) {
+    } else if (richting < 270 && richting > 225 || richting > 270 && richting < 315) {
         basic.showArrow(ArrowNames.West)
-    } else if (richting < 225) {
+    } else if (richting < 225 && richting > 180 || richting < 180 && richting > 135) {
         basic.showArrow(ArrowNames.South)
-    } else {
+    } else if (richting < 315 && richting > 90 || richting > 45 && richting < 90) {
         basic.showArrow(ArrowNames.East)
     }
 })
