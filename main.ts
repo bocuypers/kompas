1633ac2ee2c6435cadc2931ba6be8428dc54f71a
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-03-07 14:57:55 +0100

    Kompas klaar

--- main.ts
+++ main.ts
@@ -4,13 +4,13 @@ input.onButtonPressed(Button.A, function () {
 })
 basic.forever(function () {
     richting = input.compassHeading()
-    if (richting > 0 && richting < 315 || richting < 0 && richting > 45) {
-        basic.showArrow(ArrowNames.North)
-    } else if (richting < 270 && richting > 225 || richting > 270 && richting < 315) {
-        basic.showArrow(ArrowNames.West)
-    } else if (richting < 225 && richting > 180 || richting < 180 && richting > 135) {
-        basic.showArrow(ArrowNames.South)
-    } else if (richting < 315 && richting > 90 || richting > 45 && richting < 90) {
+    if (45 < richting && richting < 135) {
         basic.showArrow(ArrowNames.East)
+    } else if (richting < 225 && richting > 135) {
+        basic.showArrow(ArrowNames.South)
+    } else if (richting > 225 && richting < 315) {
+        basic.showArrow(ArrowNames.West)
+    } else if (richting < 315 && richting > 360 || richting < 45 && richting > 0) {
+        basic.showArrow(ArrowNames.North)
     }
 })
